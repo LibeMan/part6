917d64e898cd561b6be2fe768108ae22a38c9b62
Benjamin <benjamin>
2021-09-30 11:33:06 +0300

    Done

--- src/components/AnecdoteList.js
+++ src/components/AnecdoteList.js
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {createAnec} from '../reducers/anecdoteReducer'
 import { useSelector, useDispatch} from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
 import { connect } from 'react-redux'
 
+var timer
+
 const AnecdoteList = (props) => {
+  const [count, setCount] = useState(0)
+  const [yoTime, setTime] = useState(null)
 
   //Vote
   const vote = (id, anecdote) => {
+      //let yoTime = null
       const newObj = anecdote
       props.voteAnecdote(id,newObj)
-      props.setNotification(`you voted '${anecdote.content}'`, 10)
+
+      if (count === 0) {
+        clearTimeout(timer)
+        setCount(1)
+        props.setNotification(`you voted '${anecdote.content}'`, 10, count)
+        timer = setTimeout(()=> {
+          setCount(0)
+        },5000)
+        
+      } else{
+        clearTimeout(timer)
+        props.setNotification(`you voted '${anecdote.content}'`, 10, count)
+        setCount(1)
+        timer = setTimeout(()=> {
+          setCount(0)
+        },5000)
+      }
+      
   }
 
   //Sort
--- src/reducers/notificationReducer.js
+++ src/reducers/notificationReducer.js
@@ -3,6 +3,8 @@ const simpleNotes = [
     ''
 ]
 
+var timeout = null
+
 const notificationReducer = (state = simpleNotes, action) => {
     switch (action.type) {
       case 'NOTIFICATION':
@@ -17,20 +19,43 @@ const notificationReducer = (state = simpleNotes, action) => {
     return state
   }
 
-export const setNotification = (message, time) => {
+
+
+export const setNotification = (message, time, count) => {
 
   const timeWindow = time *230
 
+
   return async dispatch => {
-    dispatch({
-      type: 'NOTIFICATION',
-      text: message
-    })
-    setTimeout(() => {
-      dispatch({type: 'HIDE'})
-    }, timeWindow)
+    //HIDE
+    function hide () {
+      dispatch({type:'HIDE'})
+    }
+    //SHOW
+    function show () {
+      dispatch({
+        type:'NOTIFICATION',
+        text:message
+      })
+    }
+
+    if (count === 0){
+      show()
+      timeout = setTimeout(()=> {
+        hide()
+      }, timeWindow)
+    } else {
+      clearTimeout(timeout)
+      show()
+      timeout = setTimeout(() => {
+        hide()
+      }, timeWindow)
+    }
   }
 }
+
+
+
 
 
 export default notificationReducer
